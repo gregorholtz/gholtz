ce148fb830a42597168be3ac0d6a70eb57a1d81b
Gregor Holtz <debrisplease@gmail.com>
2015-04-16 13:49:33 -0700

    add some scss files and variables

--- footer.php
+++ footer.php
@@ -12,9 +12,7 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'gholtz' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'gholtz' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'gholtz' ), 'GHOLTZ', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+			Footer
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
--- header.php
+++ header.php
@@ -19,12 +19,9 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
-	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'gholtz' ); ?></a>
-
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
